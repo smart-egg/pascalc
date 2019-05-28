b2a0c958de0e5d761165bea27250ec40abc53eee
smart-egg-admin <50033752+smart-egg-admin@users.noreply.github.com>
2019-05-28 23:16:16 +0200

    fix file buffer for reviso

--- reviso-attachment-upload/index.js
+++ reviso-attachment-upload/index.js
@@ -1,10 +1,10 @@
 // Libraries
 const request = require('request');
 
-    // Final url
-    //const FINAL_URL = "https://prod-61.westeurope.logic.azure.com:443/workflows/a97e30c6dd51449f8def293b023363c2/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=fYabTi77sRRh4RH0HGtMbeKryTHmdB8BaoB-nfW_m38";
+// Final url
+//const FINAL_URL = "https://prod-61.westeurope.logic.azure.com:443/workflows/a97e30c6dd51449f8def293b023363c2/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=fYabTi77sRRh4RH0HGtMbeKryTHmdB8BaoB-nfW_m38";
 
-module.exports = async function (context, req) {
+module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const msg = req.body;
@@ -19,29 +19,34 @@ module.exports = async function (context, req) {
             "X-AgreementGrantToken": process.env.revisoAgreementGrantToken,
             "Content-Type": "multipart/form-data"
         }
-    }
-    
-    // Relay info to API if correctly formed
-    postOptions.formData = {
-      filename: msg.filename,
-      file: msg['file-content']
     };
 
-    context.res.statusCode = 202; // added for testing
+    // File data
+    fileBuf = new Buffer(msg['file-content'], 'base64')
+
+    // Relay info to API if correctly formed
+    postOptions.formData = {
+        filename: msg.filename,
+        file: {
+            value : fileBuf,
+            options: {
+                filename: msg.filename
+            }
+        }
+    };
 
     request(postOptions, function (err, res, reqBody) {
-        if(err) {
+        if (err) {
             context.res = {
                 status: res.statusCode,
                 body: err
-            }
-        }
-        else
-        {
+            };
+        } else {
             context.res = {
                 status: res.statusCode,
                 body: reqBody
-            }
+            };
         }
+        context.done();
     });
-}
+};
